678420ded651388af2d9b7dab6557ee60b8f3eab
Jneverson <jdneverson97@gmail.com>
2019-03-26 20:35:11 -0400

    Update opencv_detect.py

--- pyproj/opencv_detect.py
+++ pyproj/opencv_detect.py
@@ -23,11 +23,9 @@ def find_biggest_contour(image):
 	contours , hierarchy=cv2.findContours(image,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
 	contour_sizes=[(cv2.contourArea(contour),contour) for contour in contours]
-	# if(len(contour_sizes) == 0 or contour_sizes == None):
-	# 	print("None Found")
-	# 	return None
-	# else:
-	if(contour_sizes == 0):
+	
+	if not contour_sizes:
+		print("No Contours in this image")
 		return None, None
 	else:
 		biggest_contour=max(contour_sizes,key=lambda x:x[0])[1]
@@ -43,6 +41,10 @@ def find_biggest_contour(image):
 		# print("Minimum Y Value of Biggest Contour: ", np.amin(biggest_contour[1]))
 		# print("Mid Y Point: ", (np.amax(biggest_contour[1]) + np.amin(biggest_contour[1])) / 2)
 
+
+
+
+		
 		# cx, cy, area, perimeter = calculate_moments(biggest_contour)
 
 		# print("cx: %s", str(cx))
@@ -110,26 +112,26 @@ def draw_bus_stop(image):
 	image_blur_hsv=cv2.cvtColor(image_blur,cv2.COLOR_RGB2HSV)
 
 	#Green Range
-	min_color=np.array([50,80,80])
-	max_color=np.array([100,120,120])
+	min_color=np.array([50,80,130])
+	max_color=np.array([100,120,200])
 
 	mask1=cv2.inRange(image_blur_hsv,min_color,max_color)
 
 	#Red Range
-	min_color2=np.array([160,200,190])
-	max_color2=np.array([190,256,230])
+	min_color2=np.array([160,200,130])
+	max_color2=np.array([190,256,200])
 
 	mask2=cv2.inRange(image_blur_hsv,min_color2,max_color2)
 
 	#Blue Range
-	min_color3=np.array([100,200,160])
-	max_color3=np.array([135,256,200])
+	min_color3=np.array([100,200,130])
+	max_color3=np.array([135,256,20])
 
 	mask3=cv2.inRange(image_blur_hsv,min_color3,max_color3)
 
 	#Purple Range
-	min_color4=np.array([135,140,100])
-	max_color4=np.array([160,180,150])
+	min_color4=np.array([135,140,130])
+	max_color4=np.array([160,180,200])
 
 	mask4=cv2.inRange(image_blur_hsv,min_color4,max_color4)
 
@@ -139,8 +141,7 @@ def draw_bus_stop(image):
 
 	mask5=cv2.inRange(image_blur_hsv,min_color5,max_color5)
 
-	mask=mask2+mask3+mask1+mask4-mask5
-
+	mask=mask2+mask3+mask4-mask5
 	kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
 
 	mask_closed=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
@@ -148,155 +149,23 @@ def draw_bus_stop(image):
 
 	big_contour,mask_fruit=find_biggest_contour(mask_cleaned)
 
-	if(big_contour == None and mask_fruit == None):
-			return None, None
+	if(big_contour is None):
+		return None, None
+
 	else:
 		overlay=overlay_mask(mask_cleaned,image)
 
 		circled=circle_contour(overlay,big_contour)
 
-		#show(circled)
+		# show(circled)
 
 		bgr=cv2.cvtColor(circled,cv2.COLOR_RGB2BGR)
 
 		return bgr, big_contour
 
-
-def calculateDistance(x1,y1,x2,y2):
-     dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-     return dist
-
-# def draw_banana(image):
-
-# 	#PRE PROCESSING OF IMAGE
-
-# 	image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-
-# 	maxsize=max(image.shape)
-
-# 	scale=700/maxsize
-
-# 	image=cv2.resize(image,None,fx=scale,fy=scale)
-
-# 	image_blur=cv2.GaussianBlur(image,(7,7),0)
-
-# 	image_blur_hsv=cv2.cvtColor(image_blur,cv2.COLOR_RGB2HSV)
-
-# 	min_color=np.array([20,50,50])
-# 	max_color=np.array([30,256,256])
-
-# 	mask1=cv2.inRange(image_blur_hsv,min_color,max_color)
-
-# 	min_color2=np.array([60,50,50])
-# 	max_color2=np.array([70,256,256])
-
-# 	mask2=cv2.inRange(image_blur_hsv,min_color2,max_color2)
-
-# 	mask=mask1+mask2
-
-# 	kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
-
-# 	mask_closed=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
-# 	mask_cleaned=cv2.morphologyEx(mask_closed,cv2.MORPH_OPEN,kernel)
-
-# 	big_contour,mask_fruit=find_biggest_contour(mask_cleaned)
-
-# 	overlay=overlay_mask(mask_cleaned,image)
-
-# 	circled=circle_contour(overlay,big_contour)
-
-# 	show(circled)
-
-# 	bgr=cv2.cvtColor(circled,cv2.COLOR_RGB2BGR)
-
-# 	return bgr
-
-
-# def draw_strawberry(image):
-
-# 	#PRE PROCESSING OF IMAGE
-
-# 	image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-
-# 	maxsize=max(image.shape)
-
-# 	scale=700/maxsize
-
-# 	image=cv2.resize(image,None,fx=scale,fy=scale)
-
-# 	image_blur=cv2.GaussianBlur(image,(7,7),0)
-
-# 	image_blur_hsv=cv2.cvtColor(image_blur,cv2.COLOR_RGB2HSV)
-
-# 	min_color=np.array([0,100,80])
-# 	max_color=np.array([10,256,256])
-
-# 	mask1=cv2.inRange(image_blur_hsv,min_color,max_color)
-
-# 	min_color2=np.array([170,100,80])
-# 	max_color2=np.array([180,256,256])
-
-# 	mask2=cv2.inRange(image_blur_hsv,min_color2,max_color2)
-
-# 	mask=mask1+mask2
-
-# 	kernel=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
-
-# 	mask_closed=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
-# 	mask_cleaned=cv2.morphologyEx(mask_closed,cv2.MORPH_OPEN,kernel)
-
-# 	big_contour,mask_fruit=find_biggest_contour(mask_cleaned)
-
-# 	overlay=overlay_mask(mask_cleaned,image)
-
-# 	circled=circle_contour(overlay,big_contour)
-
-# 	show(circled)
-
-# 	bgr=cv2.cvtColor(circled,cv2.COLOR_RGB2BGR)
-
-# 	return bgr
-
-# def calculate_moments(contour):
-# 	cnt = contour[0]
-# 	M = cv2.moments(cnt)
-# 	print( M )
-# 	cx = int(M['m10']/M['m00'])
-# 	cy = int(M['m01']/M['m00'])
-
-# 	area = cv2.contourArea(cnt)
-
-# 	perimeter = cv2.arcLength(cnt,True)
-
-# 	return cx, cy, area, perimeter
-
-
-
-# #input image
-# bus_stop=cv2.imread('bus.jpg')
-# # banana=cv2.imread('banana.jpg')
-# # strawberry=cv2.imread('berry.jpg')
-# full_bus_stop=cv2.imread('bus_green.jpg')
-# #process image
-# result_bus_stop=draw_bus_stop(bus_stop)
-# # result_banana=draw_banana(banana)
-# # result_strawberry=draw_strawberry(strawberry)
-# result_full_bus_stop=draw_bus_stop(full_bus_stop)
-
-
-# #output image
-
-# cv2.imwrite('bus_new.jpg',result_bus_stop)
-# # cv2.imwrite('banana_new.jpg',result_banana)
-# # cv2.imwrite('strawberry_new.jpg',result_strawberry)
-# cv2.imwrite('bus_green_new.jpg',result_full_bus_stop)
-
 count = 0
 # images = [cv2.imread('photo_1.jpg'), cv2.imread('photo_2.jpg'), cv2.imread('photo_3.jpg'), cv2.imread('photo_4.jpg')
 #                     , cv2.imread('false.jpg'), cv2.imread('bus.jpg')]
-
-
-# def isBusStop(image, contour):
 
 
 # for image in glob.glob("bus_images/*.jpg") or glob.glob("bus_images/*.png"):
@@ -323,18 +192,43 @@ count = 0
 # 	count+=1
 
 
+# def calculateXDistance(image):
+
+# 	result_image, contour = draw_bus_stop(image)
+
+# 	if(result_image	== None and contour == None):
+# 		print("No contour found, distance cannot be calcualed")
+# 		return None, None
+
+# 	# contour, _ = find_biggest_contour(result_image)
+
+# 	else:
+
+# 		image_height, image_width, channels = image.shape
+
+# 		center_X = image_width/2
+# 		center_Y = image_height/2
+
+# 		cX = (np.amax(contour[0]) + np.amin(contour[0])) / 2
+# 		cY = (np.amax(contour[1]) + np.amin(contour[1])) / 2
+
+# 		print("Image Center: (" + str(center_X) + ", ", str(center_Y) + ")")
+# 		print("Bus Center: (" + str(cX) + ", " + str(cY) + ")")
+# 		distance = center_X	 - cX
+
+# 		return distance, image_width
+
 def calculateXDistance(image):
+
+	# image = cv2.imread(image)
 
 	result_image, contour = draw_bus_stop(image)
 
-	if(result_image	== None and contour == None):
-		print("No contour found, distance cannot be calcualed")
+	if(contour is None):
+		print("Distance cannot be found")
 		return None, None
 
-	# contour, _ = find_biggest_contour(result_image)
-
 	else:
-
 		image_height, image_width, channels = image.shape
 
 		center_X = image_width/2
@@ -343,10 +237,9 @@ def calculateXDistance(image):
 		cX = (np.amax(contour[0]) + np.amin(contour[0])) / 2
 		cY = (np.amax(contour[1]) + np.amin(contour[1])) / 2
 
-		print("Image Center: (" + str(center_X) + ", ", str(center_Y) + ")")
-		print("Bus Center: (" + str(cX) + ", " + str(cY) + ")")
-		distance = center_X	 - cX
 
+		distance = center_X	 - cX
+		
 		return distance, image_width
 
 
